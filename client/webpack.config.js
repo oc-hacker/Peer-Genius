88c52816fdff51d024d7c64e014cbdca0159ad3e
syy1125 <syy1125@hotmail.com>
2017-06-15 19:22:48 -0400

    Initial file setup
    Installed some npm packages central to the server.
    Initialized some files.

---
+++ client/webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var webpack = require('webpack');
+var CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+	devtool: 'cheap-module-source-map',
+	entry: [
+		'./react/main.js'
+	],
+	output: {
+		path: path.resolve(__dirname, '../public'),
+		filename: 'app.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel'
+		}]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+	    	'process.env': {
+	    		'NODE_ENV': JSON.stringify('production')
+	    	}
+	    }),
+	    new webpack.optimize.DedupePlugin(),
+	    new webpack.optimize.UglifyJsPlugin({
+	    	mangle: true,
+	    	compress: {
+	    		warnings: false,
+	    		pure_getters: true,
+	    		unsafe: true,
+	    		unsafe_comps: true,
+	    		screw_ie8: true
+	    	},
+	    	output: {
+	    		comments: false
+	    	},
+	    	exclude: [/\.min\.js$/gi]
+	    }),
+	    new webpack.optimize.AggressiveMergingPlugin(),
+	    new CompressionPlugin({
+		    asset: "[path].gz[query]",
+		    algorithm: "gzip",
+		    test: /\.js$|\.css$|\.html$/,
+		    threshold: 10240,
+		    minRatio: 0.8
+	    })
+	]
+};
---
+++ server/entry.js
@@ -0,0 +1,6 @@
+require('babel-core/register');
+require('babel-polyfill');
+
+// Any file imported using require will be transformed by babel
+
+require('./loader');
