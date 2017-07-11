6833e197b581a20edd363c5fae66f7ab9c27ada2
syy1125 <syy1125@hotmail.com>
2017-07-10 17:23:26 -0700

    CORS fix
    CORS should be fixed now.

--- server/core/server.js
+++ server/core/server.js
@@ -11,7 +11,8 @@ import apiRouter from '../router/api';
 
 const corsOptions = {
 	origin: (origin, cb) => {
-		if (origin === 'https://peergenius.io') {
+		// If origin is null or peergenius.io, it's good.
+		if (!origin || origin === 'https://peergenius.io') {
 			cb(null, true);
 		}
 		else if (config.devMode && origin === `http://localhost:${config.devServerPort}`) {
@@ -53,6 +54,7 @@ app.use('/api', apiRouter);
 // Send index
 app.get(/^\/(?!api)/, sendIndex);
 
+// Ensure responses is ended
 app.use(endResponse);
 
 // Errors
