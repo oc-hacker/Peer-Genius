b7fb98b8ef3a70802aa0918150942605bc084a65
syy1125 <syy1125@hotmail.com>
2017-07-11 18:49:58 -0700

    Fixing react hot loader removal
    React hot loader was removed in the wrong environment. That's fixed now.

--- client/index.dev.js
+++ client/index.dev.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { AppContainer } from 'react-hot-loader';
 
 import App from './react/main';
 
@@ -8,6 +9,21 @@ import App from './react/main';
 injectTapEventPlugin();
 // Render the app.
 ReactDOM.render(
-	App,
+	<AppContainer>
+		{App}
+	</AppContainer>,
 	document.getElementById('app')
 );
+
+// Hot Module Replacement
+if (module.hot) {
+	module.hot.accept('./react/main', () => {
+		const NextApp = require('./react/main').default;
+		ReactDOM.render(
+			<AppContainer>
+				{NextApp}
+			</AppContainer>,
+			document.getElementById('app')
+		)
+	})
+}
--- client/index.prod.js
+++ client/index.prod.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { AppContainer } from 'react-hot-loader';
 
 import App from './react/main';
 
@@ -9,21 +8,6 @@ import App from './react/main';
 injectTapEventPlugin();
 // Render the app.
 ReactDOM.render(
-	<AppContainer>
-		{App}
-	</AppContainer>,
+	App,
 	document.getElementById('app')
 );
-
-// Hot Module Replacement
-if (module.hot) {
-	module.hot.accept('./react/main', () => {
-		const NextApp = require('./react/main').default;
-		ReactDOM.render(
-			<AppContainer>
-				{NextApp}
-			</AppContainer>,
-			document.getElementById('app')
-		)
-	})
-}
