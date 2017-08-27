c20d90555204c24d9768996666c986ae7a891b51
Jeff Guo <jeff@Jeffs-iMac.local>
2017-08-26 20:08:31 -0700

    Guru dashboard and profile routing

---
+++ client/react/guru/profile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class GuruProfile extends ReactComponent {
+    render = () => {
+        return null;
+    }
+}
--- client/react/guru/router.js
+++ client/react/guru/router.js
@@ -4,6 +4,8 @@ import { Switch } from 'react-router';
 import { Route } from '../components';
 import SignUp from './signUp';
 import SessionsRouter from './sessions';
+import GuruDashboard from "./dashboard";
+import GuruProfile from "./profile";
 
 export default class GuruRouter extends Component {
 	constructor(props) {
@@ -15,8 +17,10 @@ export default class GuruRouter extends Component {
 		
 		return (
 			<Switch>
+				<Route exact path={`${url}`} component={GuruDashboard} />
 				<Route path={`${url}/signUp`} component={SignUp} />
 				<Route path={`${url}/sessions`} component={SessionsRouter} />
+				<Route path={`${url}/profile/:guruUUID`} component={GuruProfile} />
 			</Switch>
 		);
 	}
