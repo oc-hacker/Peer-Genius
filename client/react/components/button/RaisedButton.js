ec0fd2b2f79587313fbe858568b7c971193ded11
Jason-FTW <jyu031103@gmail.com>
2017-08-27 01:01:10 -0700

    Partially implement raised button component, add flat buttons
    ActionButtons and IconButtons are likely not needed but files are there
    in case an implementation is required later on.

---
+++ client/react/components/button/RaisedButton.js
@@ -0,0 +1,61 @@
+import React from 'React';
+
+import PropTypes from 'prop-types';
+
+import { RaisedButton } from 'material-ui/RaisedButton';
+
+/**
+ * Flat buttons (see guru_page.jpg of design)
+ */
+export default class RaisedButton extends React.Component {
+    static propTypes = {
+        label: PropTypes.string.isRequired,
+        primary: PropTypes.boolean,
+        secondary: PropTypes.boolean,
+        transparent: PropTypes.boolean
+    };
+
+    render() {
+        let {
+			label, primary, secondary, transparent,
+            ...buttonProps
+		} = this.props;
+
+        if(primary) {
+            return (
+                <RaisedButton
+                    primary
+                    label={label}
+                    buttonStyle={{ borderRadius: 5 }}
+                    style={{ borderRadius: 5 }}
+                />
+            );
+        } else if (secondary) {
+            return (
+                <RaisedButton
+                    secondary
+                    label={label}
+                    buttonStyle={{ borderRadius: 5 }}
+                    style={{ borderRadius: 5 }}
+                />
+            );
+        } else if (transparent) {
+            return (
+                <RaisedButton
+                    label={label}
+                    buttonStyle={{ borderRadius: 5, borderColor: 'black' }}
+                    style={{ borderRadius: 5, borderColor: 'black' }}
+                    labelColor={'transparent'}
+                    backgroundColor={'transparent'}
+                />
+            );
+        }
+    }
+}
+
+RaisedButton.defaultProps = {
+    label: '',
+    primary: true,
+    secondary: false,
+    transparent: false
+};
---
+++ client/react/components/button/index.js
@@ -0,0 +1,4 @@
+export { default as FlatButton } from './FlatButton';
+export { default as RaisedButton } from './RaisedButton';
+export { default as ActionButton } from './ActionButton';
+export { default as IconButton } from './IconButton';
