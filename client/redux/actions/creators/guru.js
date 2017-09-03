1be7c841e8474334f42c016d59ad5cdab2c822ec
syy1125 <syy1125@hotmail.com>
2017-09-03 12:04:55 -0700

    Add get reviews action
    Added get reviews action for retrieving reviews of a guru.

--- client/redux/actions/creators/guru.js
+++ client/redux/actions/creators/guru.js
@@ -8,20 +8,47 @@ import htttpStatus from 'http-status-codes';
 
 
 /**
+ * Thunk action creator.
  * Fetches a guru's profile from the server.
  * @param {UUID} guruUUID the guru's UUID
  */
 export const getGuruProfile = (guruUUID) => async dispatch => {
-    let response = await post('/api/schedule/getPastSessions', { guruUUID: guruUUID });
+	let response = await post('/api/schedule/getPastSessions', { guruUUID });
+	
+	if (response.ok) {
+		let guruProfile = await response.json();
+		
+		dispatch({
+			type: types.INIT_GURU_PROFILE,
+			payload: { guruProfile }
+		});
+	} else {
+		dispatch({ type: types.UNEXPECTED_ERROR });
+	}
+};
 
-    if(response.ok){
-        let guruProfile = await response.json();
-
-        dispatch({
-            type: types.INIT_GURU_PROFILE,
-            payload: { guruProfile }
-        });
-    } else {
-        dispatch({ type: types.UNEXPECTED_ERROR });
-    }
-}
+/**
+ * Thunk action creator.
+ * Fetches a guru's reviews from the server.
+ * @param guruUUID
+ */
+export const getGuruReviews = guruUUID => async dispatch => {
+	let response = await post('/api/guru/getReviews', {
+		guru: guruUUID
+	});
+	
+	if (response.ok) {
+		let { reviews } = await response.json();
+		
+		return await dispatch({
+			type: types.INIT_GURU_REVIEWS,
+			payload: {
+				guruUUID,
+				reviews
+			}
+		});
+	}
+	else {
+		return await dispatch(handleError(response));
+	}
+};
---
+++ client/redux/selectors/socket.js
@@ -0,0 +1,1 @@
+export const selectSocket = state => state.socket;
