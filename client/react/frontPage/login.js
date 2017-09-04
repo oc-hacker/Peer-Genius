e706c30df5b83aaac5974f93326073ee43535d56
syy1125 <syy1125@hotmail.com>
2017-09-04 15:02:31 -0700

    Fix log in screen
    Fixed log in screen. It should show up now.

--- client/react/frontPage/login.js
+++ client/react/frontPage/login.js
@@ -12,63 +12,55 @@ import { ReduxForm, TextField } from '../components/form';
 import { logIn } from '../../redux/actions/creators/session';
 
 const styles = ({ palette: { danger, getContrastText } }) => ({
-	buttons: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'flex-start',
-		justifyContent: 'space-between'
-	},
-	cancel: {
-		backgroundColor: danger[500],
-		color: getContrastText(danger[500]),
-		'&:hover': {
-			backgroundColor: danger[700],
-			color: getContrastText(danger[700])
-		}
-	}
+    buttons: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between'
+    }
 });
 
 @connect(null, { logIn })
 @withStyles(styles)
 export default class LoginDialog extends Component {
-	static propTypes = {
-		open: PropTypes.bool,
-		onRequestClose: PropTypes.func.isRequired
-	};
-	
-	render() {
-		let { classes, open, onRequestClose, logIn } = this.props;
-		
-		const labelWidth = '5em';
-		
-		return (
-			<Dialog open={open} onRequestClose={onRequestClose}>
-				<DialogTitle>Log In</DialogTitle>
-				<DialogContent>
-					<ReduxForm form="logIn" onSubmit={logIn}>
-						<TextField
-							name="email" label="Email" labelWidth={labelWidth}
-						/>
-						<TextField
-							name="password" type="password" label="Password" labelWidth={labelWidth}
-						/>
-						<div className={classes.buttons}>
-							<Button
-								raised className={classes.cancel}
-								onClick={onRequestClose}
-							>
-								Cancel
-							</Button>
-							<Button
-								raised color="primary"
-								type="submit"
-							>
-								Log In
-							</Button>
-						</div>
-					</ReduxForm>
-				</DialogContent>
-			</Dialog>
-		);
-	}
+    static propTypes = {
+        open: PropTypes.bool,
+        onRequestClose: PropTypes.func.isRequired
+    };
+
+    render() {
+        let { classes, open, onRequestClose, logIn } = this.props;
+
+        const labelWidth = '5em';
+
+        return (
+            <Dialog open={open} onRequestClose={onRequestClose}>
+                <DialogTitle>Log In</DialogTitle>
+                <DialogContent>
+                    <ReduxForm form="logIn" onSubmit={logIn}>
+                        <TextField
+                            name="email" label="Email" labelWidth={labelWidth}
+                        />
+                        <TextField
+                            name="password" type="password" label="Password" labelWidth={labelWidth}
+                        />
+                        <div className={classes.buttons}>
+                            <Button
+                                raised color="danger"
+                                onClick={onRequestClose}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                raised color="primary"
+                                type="submit"
+                            >
+                                Log In
+                            </Button>
+                        </div>
+                    </ReduxForm>
+                </DialogContent>
+            </Dialog>
+        );
+    }
 }
