2c13a0ccac13319c9321e4ce1d26578b7d80e648
Jason-FTW <jyu031103@gmail.com>
2017-09-04 21:46:21 -0700

    Add notification model and router function More router functions will be added

---
+++ server/database/models/notification.ts
@@ -0,0 +1,42 @@
+import * as Sequelize from 'sequelize';
+
+import { sequelizeAdmin as admin } from '../reference';
+import user from './user';
+import { ProhibitedEditError } from '../errors';
+
+export interface NotificationAttributes {
+	id?: string;
+	notification?: string;
+}
+
+export interface NotificationInstance extends Sequelize.Instance<NotificationAttributes> {
+	createdAt: Date;
+	updatedAt: Date;
+
+    id?: string;
+	notification?: string;
+}
+
+const attributes = {
+	id: {
+		type: Sequelize.UUID,
+		defaultValue: Sequelize.UUIDV4,
+		primaryKey: true
+	},
+	notification: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+};
+
+const blockUserEdit = (instance: NotificationInstance) => {
+	if (instance.changed('user')) {
+		throw new ProhibitedEditError('Editing the user FK of messages table is prohibited.');
+	}
+};
+
+const model: Sequelize.Model<NotificationInstance, NotificationAttributes> = admin.define<NotificationInstance, NotificationAttributes>('messages', attributes);
+model.beforeUpdate(blockUserEdit);
+model.sync(); // Alter when in development mode
+
+export default model;
