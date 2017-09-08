630715cece140385d9cd287e5a7657c027616905
Jason-FTW <jyu031103@gmail.com>
2017-09-07 17:07:58 -0700

    Add guru check to notifications

--- server/router/functions/notification.ts
+++ server/router/functions/notification.ts
@@ -8,7 +8,7 @@ import { AsyncHandler, VerifiedRequest } from '../../types';
 class Notification {
 	public to;
 	public message;
-	
+
 	constructor(to: string, message: string) {
 		this.to = to;
 		this.message = message;
@@ -17,7 +17,7 @@ class Notification {
 
 class NotificationInterface {
 	public open = [];
-	
+
 	/**
 	 * Adds a new Notification to the current open notification list.
 	 * @param request
@@ -25,7 +25,7 @@ class NotificationInterface {
 	public add(request: Notification): void {
 		this.open.push(request);
 	}
-	
+
 	/**
 	 * Returns the list of currently open notifications.
 	 * @return list of currently open Notifications
@@ -33,7 +33,7 @@ class NotificationInterface {
 	public getNotifications(): object[] {
 		return this.open;
 	}
-	
+
 	/**
 	 * Accepts a request and removes it from the list of open notifications.
 	 * @param {UUID} requestID UUID of the request to remove
@@ -41,7 +41,7 @@ class NotificationInterface {
 	 */
 	public acceptRequest(requestID: string): Notification {
 		for (let i = 0; i < this.open.length; i++) {
-			if (this.open[i].newbieID === requestID) {
+			if (this.open[i].newbieID === requestID || this.open[i].guruID === requestID) {
 				return this.open.splice(i, 1)[0];
 			}
 		}
