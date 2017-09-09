ea89b8ef36e31f6967ea3e6b631c4ec110557ca9
Jeff Guo <sguo35@gmail.com>
2017-09-09 01:38:18 -0700

    Fix GuruSearch styling

--- client/react/home/guruSearch/guruSearch.js
+++ client/react/home/guruSearch/guruSearch.js
@@ -17,33 +17,6 @@ let styles = {
   },
   centerText: {
     textAlign: 'center'
-  },
-  textPadding: {
-    paddingTop: 10,
-    paddingBottom: 10
-  },
-  coloredBar: {
-    background: 'linear-gradient(to bottom, #f4fc00 0%,#00aeb7 100%)',
-    borderRadius: 5,
-    zIndex: 10
-  },
-  emptyBar: {
-    boxShadow: 'inset 3px 3px 3px 3px gray',
-    borderRadius: 5,
-    border: '1px solid gray'
-  },
-  leftPadding1: {
-    marginleft: 15
-  },
-  darkGreyBackground: {
-    backgroundColor: 'rgb(244, 244, 244)'
-  },
-  lightGreyBackground: {
-    backgroundColor: 'rgb(252, 252, 252)'
-  },
-  sidePadding: {
-    paddingLeft: 300,
-    paddingRight: 300
   }
 };
 
@@ -60,16 +33,6 @@ export default class GuruSearch extends React.Component {
             className={classNames(classes.centerText, classes.headerText)}>Eliminate</Text>
           <Text type='display1' color='black' className={classNames(classes.centerText, classes.headerText)}>The
             Grind</Text>
-        </div>
-        <div className={classNames(classes.sidePadding)}>
-          <span>
-            <div className={classNames(classes.darkGreyBackground)}>
-              <Text type='subheading' color='black' className={classes.leftPadding1}>Notification</Text>
-            </div>
-            <div className={classNames(classes.lightGreyBackground)}>
-              {notifications}
-            </div>
-          </span>
         </div>
       </div>
     );
