b4ef3a37afb721bf103a1754f10d6f50415b7cb5
Jeff Guo <sguo35@gmail.com>
2017-09-09 01:22:11 -0700

    Style fixes

--- client/react/newbie/newbieDashboard.js
+++ client/react/newbie/newbieDashboard.js
@@ -20,42 +20,6 @@ let styles = {
   },
   centerText: {
     textAlign: 'center'
-  },
-  textPadding: {
-    paddingTop: 10,
-    paddingBottom: 10
-  },
-  leftPadding1: {
-    marginLeft: 15,
-    paddingTop: 7,
-    paddingBottom: 7
-  },
-  darkGreyBackground: {
-    backgroundColor: 'rgb(244, 244, 244)',
-    width: 200
-  },
-  lightGreyBackground: {
-    backgroundColor: 'rgb(252, 252, 252)',
-    height: 300
-  },
-  sidePadding: {
-    paddingLeft: 300,
-    paddingRight: 300
-  },
-  paddingLeft: {
-    paddingLeft: 30
-  },
-  roundedDot: {
-    width: 10,
-    borderRadius: '50%',
-    backgroundColor: 'black',
-    paddingBottom: 10,
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  },
-  inlineDiv: {
-    display: 'inline-block',
-    float: 'left'
   }
 };
 
@@ -74,66 +38,13 @@ export default class NewbieDashboard extends React.Component {
         <div className={classNames(classes.headerText, classes.headerBackground)}>
           <Text type='display1' color='white' className={classNames(classes.centerText, classes.headerText)}>YOUR
             DASHBOARD</Text>
-          <div style={{ display: 'inline-block', borderStyle: '2px solid', borderColor: 'white' }}>
+          <div style={{ display: 'block', borderStyle: 'solid', borderColor: 'white', marginLeft: 'auto', marginRight: 'auto', width: '20%' }}>
             <Button
               flat color="primary"
               onClick={() => push('/home/schedule')}
             >
               <Text type="button" color='white' weight='bold'>REQUEST FOR ANOTHER SESSION</Text>
             </Button>
-          </div>
-        </div>
-        <div style={{ width: '100%', height: 20 }} />
-        <div className={classNames(classes.sidePadding)}>
-          <div className={classNames(classes.inlineDiv)}>
-            <div className={classNames(classes.darkGreyBackground)}>
-              <Text type='subheading' color='black' className={classes.leftPadding1}>Notification</Text>
-            </div>
-            <div className={classNames(classes.lightGreyBackground)}>
-              {notifications}
-            </div>
-          </div>
-          <div className={classNames(classes.inlineDiv, classes.paddingLeft)}>
-            <Text type='title' color='black' className={classes.textPadding}>PREVIOUS SESSION</Text>
-            <ProgressBar.Line progress={(currentHours / 250)} text={currentHours + ' hours'} options={{
-              color: 'rgb(1,147,172)',
-              strokeWidth: 6
-            }} initialAnimate={true} containerStyle={{ width: 600, height: 35, marginBottom: 15 }} />
-            <Text
-              type='subheading' color='rgb(1,147,172)'
-              className={classNames(classes.inlineDiv, classes.textPadding, classes.centerText)}
-              style={{ marginLeft: 30 }}>
-              <div className={classes.roundedDot} />
-              <br />GURU
-            </Text>
-            <Text
-              type='subheading' color='rgb(1,147,172)'
-              className={classNames(classes.inlineDiv, classes.textPadding, classes.centerText)}
-              style={{ marginLeft: 90 }}>
-              <div className={classes.roundedDot} />
-              <br />Bronze<br />Assignment
-            </Text>
-            <Text
-              type='subheading' color='rgb(1,147,172)'
-              className={classNames(classes.inlineDiv, classes.textPadding, classes.centerText)}
-              style={{ marginLeft: 90 }}>
-              <div className={classes.roundedDot} />
-              <br />Silver<br />Courses
-            </Text>
-            <Text
-              type='subheading' color='rgb(1,147,172)'
-              className={classNames(classes.inlineDiv, classes.textPadding, classes.centerText)}
-              style={{ marginLeft: 90 }}>
-              <div className={classes.roundedDot} />
-              <br />Silver<br />Courses
-            </Text>
-            <Text
-              type='subheading' color='rgb(1,147,172)'
-              className={classNames(classes.inlineDiv, classes.textPadding, classes.centerText)}
-              style={{ marginLeft: 90 }}>
-              <div className={classes.roundedDot} />
-              <br />Gold<br />Length
-            </Text>
           </div>
         </div>
       </div>
