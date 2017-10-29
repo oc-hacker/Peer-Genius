18ccdec86f7ef901d162646b684f83b3bd803364
Jeff <sugo35@gmail.com>
2017-10-29 11:07:58 -0700

    Become a guru

--- client/react/guru/signUp/signUp.js
+++ client/react/guru/signUp/signUp.js
@@ -7,24 +7,37 @@ import { Button, Flex, Paper } from '../../components';
 import { DateField, ReduxForm, SelectField } from '../../components/form';
 
 import { range, required } from '../../components/form/validator';
-import { getGuruProfile } from '../../../redux/actions/creators/guru';
+import { update } from '../../../redux/actions/creators/guru';
 import { selectUser } from '../../../redux/selectors/user';
 
+import { formValueSelector } from 'redux-form';
+
+const formSelector = formValueSelector('becomeAGuru');
+
 @connect(state => ({
-  user: selectUser(state)
+  user: selectUser(state),
+  selectedCourse: formSelector(state, 'course'),
+  courses: state.config.courses ? state.config.courses.courses : []
 }), {
-  getGuruProfile
+  update
 })
 export default class SignUp extends Component {
   render() {
+    let { update, user, selectedCourse } = this.props;
+
+    let courses = typeof this.props.courses === 'object' ? this.props.courses.map(course => { return {
+      value: course.id,
+      label: course.name
+    }}) : [{value: "Loading", label: "Loading"}];
+
     let minDate = new Date(), maxDate = new Date();
     minDate.setFullYear(minDate.getFullYear() - 19);
     maxDate.setFullYear(maxDate.getFullYear() - 13);
 
     return (
-      <Paper>
+      <Paper style={{width: '100%', padding: 50}}>
         <Typography type="title">Fill out the Form</Typography>
-        <ReduxForm form="becomeAGuru" onSubmit={console.log}>
+        <ReduxForm form="becomeAGuru" onSubmit={() => update(user.id, selectedCourse, true)}>
           <DateField
             name="birthdate"
             label="Date of Birth"
@@ -42,8 +55,7 @@ export default class SignUp extends Component {
             })`You must be between 13 years and 18 years of age to participate at Peer Genius.`]}
           />
           {/*<SubjectFields />*/}
-          <SelectField name="todo" />
-          <SelectField />
+          <SelectField name="course" label="Area of Expertise" options={courses}/>
           <Flex direction="row-reverse">
             <Button color="primary" type="submit">Done</Button>
           </Flex>
