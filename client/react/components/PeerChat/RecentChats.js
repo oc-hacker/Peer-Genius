d353b2735c19310a346ef746922a5feb94ae3c7f
Jeff Guo <sguo35@gmail.com>
2018-01-15 09:12:50 -0800

    Reverse recent chat sorting order

--- client/react/components/PeerChat/RecentChats.js
+++ client/react/components/PeerChat/RecentChats.js
@@ -104,7 +104,7 @@ export default class RecentChats extends Component {
     }
 
     recent.sort((a, b) => {
-      return (new Date(b.time)).getTime() - (new Date(a.time)).getTime();
+      return (new Date(a.time)).getTime() - (new Date(b.time)).getTime();
     });
 
     return (
