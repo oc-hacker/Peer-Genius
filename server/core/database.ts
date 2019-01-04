1a356471781dd571c53ed2059a4aff4d3552a6db
Jason-FTW <jyu031103@gmail.com>
2019-01-04 10:57:56 -0800

    Patch sequelize model order

--- server/core/database.ts
+++ server/core/database.ts
@@ -1,28 +1,31 @@
-import * as async from 'async';
+// import * as async from 'async';
 
 import * as models from '../database/models';
+// import { sequelizeAdmin } from '../database/reference';
 
 const order = [models.user, models.account, models.session, models.message, models.key, models.guru];
-let index = 0;
+// let index = 0;
 
 const eachSeries = async () => {
+	console.log('Beginning database sync.');
 	for (let model of order) {
-		await model.sync();
+		await model.sync({ logging: console.log });
 	}
+	console.log('Database sync complete.');
 };
+
+// eachSeries();
 
 /* async.eachSeries(order, async model => {
 	await model
-			.sync()
+			.sync({ logging: console.log })
 			.then(() => {
 					console.log(`Synced ${index}`);
 					index ++;
-					return true;
 			});
 }, (err, contents) => {
 	if (err) {
 		throw err;
 	}
 	console.log('completed migration');
-});
-*/
+}); */
