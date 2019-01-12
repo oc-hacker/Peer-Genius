236a38f31d1b7426d8fce37775d1b43eb1541126
Jason-FTW <jyu031103@gmail.com>
2019-01-12 09:13:11 -0800

    Add additional logging for logins and redirect

--- client/redux/actions/creators/session.js
+++ client/redux/actions/creators/session.js
@@ -15,15 +15,15 @@ import { handleStore } from './utils';
  */
 export const logIn = credentials => async dispatch => {
   let response = await post('/api/login', credentials);
-  console.log(response);
   if (response.ok) {
     // Log in successful. Save store and redirect to user page.
     let json = await response.json();
-
+    console.log(json);
     dispatch(handleStore(json));
     if (json.isGuru){
       dispatch(push('/guru'));
     } else {
+      console.log('Redirecting to newbie dashboard');
       dispatch(push('/newbie'));
     }
   }
