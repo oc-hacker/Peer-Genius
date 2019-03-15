4f63e731cd15567370d35a9ff453c10a41f6c6ee
Jason-FTW <jyu031103@gmail.com>
2019-03-15 16:22:37 -0700

    hide elements of livestream dashboard

--- client/react/newbie/router.js
+++ client/react/newbie/router.js
@@ -14,7 +14,7 @@ export default class NewbieRouter extends Component {
 
     return (
       <Switch>
-        <Route exact path={`${url}`} component={StreamDashboard} />
+        <Route exact path={`${url}`} component={NewbieDashboard} />
         <Route path={`${url}/sessions`} component={SessionsRouter} />
         <Route path={`${url}/schedule`} component={Schedule} />
         <Route path={`${url}/finding`} component={Finding} />
